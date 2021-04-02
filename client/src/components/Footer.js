721222c10ce1c91d4c45902c54b0daeb558cc989
TanyaRan <tatyanaran@gmail.com>
2021-04-02 21:50:37 +0300

    add Header, Footer, use react-bootstrap

---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className='text-center py-3'>Copyright &copy; Learn Easy</Col>
+      </Row>
+    </Container>
+    // <footer>
+    //   <h3>Footer</h3>
+    // </footer>
+  )
+}
+
+export default Footer
