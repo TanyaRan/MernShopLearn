c6917ec5297a5b9693d37c191a3ca1f2be7f4439
TanyaRan <tatyanaran@gmail.com>
2021-04-07 14:19:31 +0300

    generate token, add auth middleware

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { authUser } from '../controllers/userCtrl.js'
+import { authUser, getUserProfile } from '../controllers/userCtrl.js'
+import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
+router.get('/profile', protect, getUserProfile)
 
 export default router
