b000cb72432e9ced4aaf9eb8a7aca20fbf13fdae
TanyaRan <tatyanaran@gmail.com>
2021-04-12 16:23:48 +0300

    BE - getOrderById implement

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addOrderItems } from '../controllers/orderCtrl.js'
+import { addOrderItems, getOrderById } from '../controllers/orderCtrl.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.post('/', protect, addOrderItems)
+router.get('/:id', protect, getOrderById)
 
 export default router
