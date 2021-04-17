199ea44cafa27c9e7d1dea8513bb73a900586c3e
TanyaRan <tatyanaran@gmail.com>
2021-04-17 03:16:43 +0300

    add .env(for a day), fix UI

--- client/src/screens/AboutScreen.js
+++ client/src/screens/AboutScreen.js
@@ -8,7 +8,6 @@ const AboutScreen = () => {
     <>
       <ProductCarousel />
       <div className='text-center'>
-        <h1>Happy in Wild</h1>
         <h5>
           We are a team of enthusiasts, specialists in the field of mountain
           tourism, organized trips and trainings in nature. The professionalism
@@ -19,17 +18,16 @@ const AboutScreen = () => {
         <h4>
           Perfectly organized trips, with correctly chosen routes and last but
           not least with a good price! Young and experienced tourists choose our
-          services for their active vacation in the mountains.
+          services for their active vacation in the mountains. This is an
+          unforgettable delight of the senses ...
         </h4>
         <h5>
           We have accumulated experience in holding corporate events, seminars,
-          and sports competitions.
-        </h5>
-        <h5>
-          We revived the children's suburban camps and created the Mountain
-          School "Wild Goats", which enjoys great interest among children. We
-          also organize mountain children's camps, where children learn
-          knowledge and skills about living in the mountains.
+          and sports competitions. We revived the children's suburban camps and
+          created the Mountain School "Wild Goats", which enjoys great interest
+          among children. We also organize mountain children's camps, where
+          children learn knowledge and skills about living in the mountains.
+          Come and enjoy the moment, communicate with the wildlife fully ...
         </h5>
         <LinkContainer to='/'>
           <Button variant='light' className='mr-3'>
